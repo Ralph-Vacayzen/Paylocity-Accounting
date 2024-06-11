a6953eef070245ecd333dc09b4bc2a6ef2e3256f
Ralph McCracken <ralph@mccracken.work>
2024-06-11 11:11:46 -0500

    working version

---
+++ main.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+
+st.caption('VACAYZEN')
+st.title('Paylocity Accounting')
+st.info('Remove unnecessary data from a paylocity export.')
+
+file = st.file_uploader('Payroll Register Data Export', 'CSV')
+
+if file is not None:
+    columns = [
+        'Co',
+        'Location',
+        'Employee',
+        'ID',
+        'Process',
+        'Check Date',
+        'Net',
+        
+        'E-REGHours',
+        'E-REGAmount',
+        'E-OTHours',
+        'E-OTAmount',
+        'E-PTOHours',
+        'E-PTOAmount',
+        'E-SICKMAmount',
+        'E-SICKHours',
+        'E-SICKAmount',
+        'E-COMMHours',
+        'E-COMMAmount',
+        'E-HOLHours',
+        'E-HOLAmount',
+        'E-BRVMTHours',
+        'E-BRVMTAmount',
+        'E-REIMHours',
+        'E-REIMAmount',
+        'E-RETROHours',
+        'E-RETROAmount'
+    ]
+
+    df = pd.read_csv(file, index_col=False)
+    df = df[columns]
+
+    st.download_button('DOWNLOAD', df.to_csv(index=False), 'Paylocity_Export.csv', 'CSV', use_container_width=True, type='primary')
